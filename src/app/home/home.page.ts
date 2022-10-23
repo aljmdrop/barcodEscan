943676e09cfc66e2e5f9b34bcfa81d7634c25cf8
daniel villamziar <danielalejandrovillamizar@gmail.com>
2022-10-22 23:04:57 -0300

    commit2

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  code: any
+  constructor(private barcodeScanner: BarcodeScanner) {}
+  ngOnInit(){
+
+  }
+  scan()
+  {
+    this.barcodeScanner.scan().then(barcodeData => {
+    this.code=barcodeData.text;
+    console.log('Barcode data', this.code);
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+  
+  
+
+}
